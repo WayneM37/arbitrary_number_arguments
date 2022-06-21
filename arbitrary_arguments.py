3a62ab7aec1c668216aafcae0c7f32aad8b0cff6
Wayne <106159269+WayneM37@users.noreply.github.com>
2022-06-21 11:39:26 +0300

    Update arbitrary_arguments.py

--- arbitrary_arguments.py
+++ arbitrary_arguments.py
@@ -18,3 +18,21 @@ def org(** data):
   print("names: ", nam)
   print("ages: ", age)
 org(beth=26, tak=34, tik=45, tok=34,fat=43)
+
+# arbitrary number of arguments **kwargs
+def animals(**ahanda):
+  for key, value in ahanda.items():
+    print(value, "are", key)
+
+animals(carnivores ="lions", omnivores ="bears", herbivores="Deers", nimnivores="human")
+
+#arbitrary number of arguments **kwargs
+def organizer(**ahanda):
+  names=[]
+  ages= []
+  for key, values in ahanda.items():
+    names.append(key)
+    ages.append(values)
+  print(names, ages, sep="\n")
+
+organizer (beth=26, tak=34, tik=45, tok=34,fat=43)
